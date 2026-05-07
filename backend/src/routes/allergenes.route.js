f72ab8aeba7a0c922e6bfea1b676acb4a7834212
Vero2583 <vfraigneau64@gmail.Com>
2026-05-07 11:09:22 +0200

    correction fichier allergenes

--- backend/src/routes/allergenes.route.js
+++ backend/src/routes/allergenes.route.js
@@ -1,15 +1,15 @@
 import express from 'express'
 import { createAllergenes, getAllergenes, getAllergenesById, updateAllergenesById, deleteAllergenesById } from '../controllers/allergenes.controller.js'
-import { authMiddleware } from '../middlewares/auth.middleware.js';
+
 
 
 const router = express.Router();
 
 router.get("/", getAllergenes)
 router.get("/:id", getAllergenesById)
-router.post("/", authMiddleware,  createAllergenes)
-router.put("/:id", authMiddleware, updateAllergenesById)
-router.put("/:id", authMiddleware, deleteAllergenesById)
+router.post("/", createAllergenes)
+router.put("/:id", updateAllergenesById)
+router.put("/:id", deleteAllergenesById)
 
 
 
