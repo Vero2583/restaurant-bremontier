3bcf7aea1a8c117efd767b81c4f8fceef5bd2a0f
Vero2583 <vfraigneau64@gmail.Com>
2026-06-10 13:26:09 +0200

    cors change deploiment

--- frontend/src/data/restaurant.js
+++ frontend/src/data/restaurant.js
@@ -1,43 +1,43 @@
 const cards = [
     {
-      imgSrc: "/cartes/oeufs mimosas.png",
+      imgSrc: "/cartes/oeufs mimosas.webp",
       imgAlt: "les entrées",
       title: "Carte des entrees",
       buttonTitle: "Voir les entrees"
     },
     {
-      imgSrc: "/cartes/plats.png",
+      imgSrc: "/cartes/entrecote.webp",
       imgAlt: "les plats",
       title: "Carte des plats",
       buttonTitle: "Voir les plats"
     },
     {
-      imgSrc: "https://placehold.co/120x140",
+      imgSrc: "/cartes/2_desserts.webp",
       imgAlt: "Carte des desserts",
       title: "Carte des desserts",
       buttonTitle: "Voir les desserts"
     },
     {
-      imgSrc:"https://placehold.co/120x140",
+      imgSrc:"/cartes/.webp",
       imgAlt:"Carte et menu enfant",
       title:"Carte et menu enfant",
       buttonTitle:"Voir la carte et menu enfant"
     },
     {
-      imgSrc:"https://placehold.co/120x140",
+      imgSrc:"/cartes/menu_à_22_euros.webp",
       imgAlt:"Menus à 22€",
       title:"Menus à 22€",
       buttonTitle:"Voir le menu 22€"
     },
     {
-      imgSrc:"https://placehold.co/120x140",
+      imgSrc:"/cartes/menu_à_26_euros.webp",
       imgAlt:"Menus à 26€",
       title:"Menus à 26€",
       buttonTitle:"Voir le menu 26€"
     },
     {
-      imgSrc:"https://placehold.co/120x140",
-      imgAlt:"",
+      imgSrc:"/bar/biere_cacaouette.webp",
+      imgAlt:"une biere avec des cacaouttes",
       title:"Les boissons",
       buttonTitle:"Voir les boissons"
     },
